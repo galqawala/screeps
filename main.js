2c95c4fa46ff2735c5ea94ffd8d1c640bba79169
galqawala <github.prayerlessly@aleeas.com>
2022-09-20 21:21:59 +0300

    debug

--- main.js
+++ main.js
@@ -249,6 +249,7 @@ function getDestinationFromMemory(creep) {
 }
 
 function handleCreep(creep) {
+    msg(creep, 'handleCreep()');
     if (creep.memory.role === 'upgrader' || creep.memory.role === 'harvester') creep.memory.role === 'worker';
     if (creep.spawning) return;
 
