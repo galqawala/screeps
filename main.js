7140e3f17fc1561ba8cfae1482d4cd68c48b39b9
galqawala <github.prayerlessly@aleeas.com>
2022-09-20 21:29:17 +0300

    remove debug message

--- main.js
+++ main.js
@@ -249,7 +249,6 @@ function getDestinationFromMemory(creep) {
 }
 
 function handleCreep(creep) {
-    msg(creep, 'handleCreep()');
     if (creep.memory.role === 'upgrader' || creep.memory.role === 'harvester') creep.memory.role === 'worker';
     if (creep.spawning) return;
 
