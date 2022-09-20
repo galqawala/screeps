bc177069a833f77a0866fda7510f62ccd7879a6b
galqawala <github.prayerlessly@aleeas.com>
2022-09-20 22:39:22 +0300

    debug

--- main.js
+++ main.js
@@ -547,7 +547,7 @@ function getBuildTaskInRange(pos) {
 function getTransferTaskInRange(pos) {
     let sources = pos.findInRange(FIND_SOURCES, { filter: (source) => { return getEnergy(source) > 0; } });
     if (sources.length < 1) {
-        msg(pos, 'nothing to harvest, no need to transfer');
+        if (pos.x === 4 && pos.y === 11) msg(pos, 'nothing to harvest, no need to transfer');
         return; //no need to transfer
     }
 
