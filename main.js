178187f8e573e81695639794236033245b70dc72
galqawala <github.prayerlessly@aleeas.com>
2022-09-21 19:58:51 +0300

    debug

--- main.js
+++ main.js
@@ -637,9 +637,11 @@ function getAvailableHarvestSpots(room) {
     let availableSpots = [];
 
     spots.forEach(spot => {
-        msg(room, 'spot x: ' + spot.x + ' y: ' + spot.y + ' roomName: ' + spot.roomName);
         let pos = new RoomPosition(spot.x, spot.y, spot.roomName);
-        if (!pos) msg(room, 'getAvailableHarvestSpots() missing pos for spot: ' + Object.keys(spot));
+        if (!pos) {
+            msg(room, 'getAvailableHarvestSpots() missing pos for spot: ' + Object.keys(spot));
+            msg(room, 'spot x: ' + spot.x + ' y: ' + spot.y + ' roomName: ' + spot.roomName);
+        }
         if (pos.findInRange(FIND_SOURCES_ACTIVE, 1).length >= 1
             && pos.lookFor(LOOK_CREEPS).length < 1
             && !creepsOnWayToPos(pos)) {
