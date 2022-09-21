a51f58bb83c2408144d2c14244b8cb82c95d3e6a
galqawala <github.prayerlessly@aleeas.com>
2022-09-21 20:05:14 +0300

    debug

--- main.js
+++ main.js
@@ -641,7 +641,7 @@ function getAvailableHarvestSpots(room) {
         msg(room, 'spot x: ' + spot.x + ' y: ' + spot.y + ' roomName: ' + spot.roomName);
         msg(room, 'pos x: ' + pos.x + ' y: ' + pos.y + ' roomName: ' + pos.roomName);
         if (pos.findInRange(FIND_SOURCES_ACTIVE, 1).length >= 1
-            && pos.lookFor(LOOK_CREEPS).length < 1
+            //&& pos.lookFor(LOOK_CREEPS).length < 1
             /*&& !creepsOnWayToPos(pos)*/) {
 
             availableSpots += spot;
